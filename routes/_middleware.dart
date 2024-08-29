9f96edd29de5999c81c4ce74b2533b0573b743d8
Fhriii <muhammadfahrrri@gmail.com>
2024-08-29 16:46:16 +0700

    nanana

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,7 +1,7 @@
-
 import 'package:dotenv/dotenv.dart';
 import 'package:dart_frog/dart_frog.dart';
 import 'package:codock/src/generated/prisma/prisma_client.dart';
+import 'package:dart_frog_cors/dart_frog_cors.dart';
 
 final env = DotEnv()..load();
 
@@ -14,5 +14,26 @@ final prisma = PrismaClient(
 Handler middleware(Handler handler) {
   return handler
     .use(requestLogger())
-    .use(provider<PrismaClient>((context) => prisma));
+    .use(cors(
+      allowOrigin: '*', // Mengizinkan semua origin
+      allowHeaders: 'Content-Type, Authorization', // Daftar header yang diizinkan
+      allowMethods: 'GET, POST, PUT, DELETE, OPTIONS', // Daftar metode yang diizinkan
+    ))
+    .use(provider<PrismaClient>((context) => prisma))
+    .use((handler) {
+      return (context) async {
+        final response = await handler(context);
+        if (context.request.method == HttpMethod.options) {
+          return response.copyWith(
+            headers: { 
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+              'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            },
+          );
+        }
+        return response;
+      };
+    });
 }
+
